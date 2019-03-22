cbdf0b3c41d76b6225cd15f7f20d3e7b0f431176
Lucas <lucas.vbotelho83@gmail.com>
2019-03-22 17:30:40 -0300

    Removed T2 only current engine state

--- 170109071_T2/src/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Game.h"
-
-int main(int argc,const char * argv[]){
-    std::cout << argc << argv << "\n" << std::endl;
-    Game game = Game::GetInstance();
-    game.Run();
-    return 0;
-}
