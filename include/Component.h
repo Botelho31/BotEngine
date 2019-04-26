08d364f9e88b484d9d2670f264a7d736c9aa86c1
Lucas <lucas.vbotelho83@gmail.com>
2019-04-26 12:19:19 -0300

    faulty freeze applied in component

--- include/Component.h
+++ include/Component.h
@@ -4,19 +4,44 @@
 
 #ifndef COMPONENT_H
     #define COMPONENT_H
+    #include "Timer.h"
     class GameObject;
 
     class Component{
         public:
-            Component(GameObject& associated) : associated(associated){};
-            virtual ~Component() {};
+            Component(GameObject& associated) : associated(associated){
+                freeze = false;
+                freezetime = 0;
+                freezetimer = new Timer();
+            };
+            virtual ~Component() {
+                delete freezetimer;
+            };
             virtual void Start() {};
             virtual void Update(float dt) = 0;
             virtual void Render() = 0;
             virtual bool Is(std::string type) = 0;
             virtual void NotifyCollision (GameObject& other) {};
+            virtual void KeepStill(bool freeze,float time = 0) {};
+
+            bool GetFreeze(){
+                return freeze;
+            }
+            void UpdateFreeze(float dt) {
+                if(freezetimer->Started()){
+                    freezetimer->Update(dt);
+                    if(freezetimer->Get() >= freezetime){
+                        freeze = false;
+                        freezetime = 0;
+                        freezetimer->Restart();
+                    }
+                }
+            };
         protected:
             GameObject& associated;
+            bool freeze;
+            float freezetime;
+            Timer *freezetimer;
     };
 
 #endif
