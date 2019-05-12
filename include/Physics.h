3c32687a95f7f06537ee623207e4a4aaa0491d86
Lucas <lucas.vbotelho83@gmail.com>
2019-05-11 21:06:35 -0300

    removed old canmove and modified distanceto

--- include/Physics.h
+++ include/Physics.h
@@ -15,13 +15,12 @@
             ~Physics();
             void Update(Rect collider,int max = 150);
             void CorrectDistance();    //Correct the distance if the player is inside a wall
-            int DistanceTo(Vec2 vector1,Vec2 vector2,int xsum,int ysum,int max = 150);    //Gets the distance to a unpassable tile block in a direction
-            bool CanMove(Vec2 vector1,Vec2 vector2);    //Checks it the side of a rect can move
+            int DistanceTo(Rect box,int xsum,int ysum,int max = 150);    //Gets the distance to a unpassable tile block in a direction
             int DistanceTo(Vec2 vector,Vec2 vectorTo,int max = 150); // Gets the from one vector to another throught the tilemap
             bool CanMove(Vec2 vector);    //Checks it vector can move
 
             //NEW FUNCTIONS
-            bool IsColliding(Rect box,float angle = 0,bool warncollision = false); //Checks to see if it is colliding with tilecolliders
+            bool IsColliding(Rect box,float angle = 0); //Checks to see if it is colliding with tilecolliders
             bool IsRight(int sum = 1);
             bool IsUp(int sum = 1);
             bool IsLeft(int sum = 1);
