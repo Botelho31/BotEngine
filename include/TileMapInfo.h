17ab32352226c5156e26aed50196d970b88e45fe
Lucas <lucas.vbotelho83@gmail.com>
2019-05-12 00:29:17 -0300

    removed movingtiles

--- include/TileMapInfo.h
+++ include/TileMapInfo.h
@@ -13,7 +13,6 @@
             void Open(std::string file);
             std::map<int,Vec2> portals;     //Keeps where the player will be teleported
             std::map<int,std::vector<std::string>> portalfiles; //Keeps the files of the new map
-            std::vector<std::weak_ptr<GameObject>> movingtiles; //Keeps all the movingtiles in the map
     };
 
 #endif
