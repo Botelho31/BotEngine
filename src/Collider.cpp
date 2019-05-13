fb316332d3b1ae0f54caf5b082c50fb03f5eadbb
Lucas <lucas.vbotelho83@gmail.com>
2019-05-13 01:01:06 -0300

    passed render to windoweffects

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -1,6 +1,7 @@
 #include "../include/Collider.h"
 #include "../include/Camera.h"
 #include "../include/InputManager.h"
+#include "../include/WindowEffects.h"
 
 Collider::Collider(GameObject& associated,Vec2 scale,Vec2 offset) : 
     Component(associated),scale(scale),offset(offset){
@@ -19,28 +20,7 @@ void Collider::Render() {
 #ifdef DEBUG
 	InputManager *input = &(InputManager::GetInstance());
 	if(input->IsKeyDown(SDLK_EQUALS)){
-		Vec2 center( box.GetCenter() );
-		SDL_Point points[5];
-
-		Vec2 point = (Vec2(box.x, box.y) - center).GetRotated( associated.angleDeg/(180/PI) )
-						+ center - Camera::pos;
-		points[0] = {(int)point.x, (int)point.y};
-		points[4] = {(int)point.x, (int)point.y};
-		
-		point = (Vec2(box.x + box.w, box.y) - center).GetRotated( associated.angleDeg/(180/PI) )
-						+ center - Camera::pos;
-		points[1] = {(int)point.x, (int)point.y};
-		
-		point = (Vec2(box.x + box.w, box.y + box.h) - center).GetRotated( associated.angleDeg/(180/PI) )
-						+ center - Camera::pos;
-		points[2] = {(int)point.x, (int)point.y};
-		
-		point = (Vec2(box.x, box.y + box.h) - center).GetRotated( associated.angleDeg/(180/PI) )
-						+ center - Camera::pos;
-		points[3] = {(int)point.x, (int)point.y};
-
-		SDL_SetRenderDrawColor(Game::GetInstance().GetRenderer(), 255, 0, 0, SDL_ALPHA_OPAQUE);
-		SDL_RenderDrawLines(Game::GetInstance().GetRenderer(), points, 5);
+		WindowEffects::DrawBox(box,associated.angleDeg/(180/PI),255,0,0);
 	}
 #endif // DEBUG
 }
