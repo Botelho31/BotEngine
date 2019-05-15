0dd536b2013b3f2237eaf55cc8f19660ccc96c2f
Lucas <lucas.vbotelho83@gmail.com>
2019-05-15 01:31:23 -0300

    rect now has equal operator

--- include/Rect.h
+++ include/Rect.h
@@ -19,8 +19,15 @@
             float GetDistance(float x,float y);
             Vec2 GetCenter();
             float GetAngle(float x,float y,float offsetX = 0,float offsetY = 0);
-            // bool Follow(float x,float y,float speedX,float speedY,float dt);
             float w,h,x,y;
+
+            bool operator==(const Rect& rhs) const {
+                if((x == rhs.x) && (y == rhs.y) && (w == rhs.w) && (h == rhs.h)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
     };
 
 #endif
