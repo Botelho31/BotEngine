db6a123cfb98a8d7a017d70a06d8d1afe585396f
Botelho31 <lucas.vbotelho83@gmail.com>
2019-06-04 19:52:15 -0300

    changed room-1

---
+++ assets/map/room-1background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="room-1background" tilewidth="50" tileheight="50" tilecount="3300" columns="55">
+ <image source="../img/fundos/room 1.png" trans="ff00ff" width="2750" height="3000"/>
+</tileset>
