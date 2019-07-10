c310a95a273fdd4318c113e84a7b1b7182fec061
Botelho31 <lucas.vbotelho83@gmail.com>
2019-07-10 09:35:41 -0300

    added some backgrounds

---
+++ assets/map/entryroomassets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="entryroomassets" tilewidth="50" tileheight="50" tilecount="3360" columns="84">
+ <image source="../img/fundos/EntryRoom Front.png" trans="ff00ff" width="4200" height="2000"/>
+</tileset>
