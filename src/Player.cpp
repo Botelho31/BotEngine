dad601168d6bb5f8fd4f80c1a5f1e9b6268d61d9
Botelho31 <lucas.vbotelho83@gmail.com>
2019-07-11 22:04:56 -0300

    put in death colored

--- src/Player.cpp
+++ src/Player.cpp
@@ -718,7 +718,7 @@ void Player::HealPlayer(int heal){
 void Player::KillPlayer(){
     Camera::UnFollow();
     GameObject *deadObj = new GameObject();
-    Sprite *deadsprite = new Sprite(*deadObj,spritefiles["dead"],17,0.06,0,false);
+    Sprite *deadsprite = new Sprite(*deadObj,spritefiles["dead"],28,0.06,0,false);
     int xoffset = -40;
     if(playersprite->IsFlipped()){
         deadsprite->Flip();
